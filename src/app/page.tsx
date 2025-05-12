ca20b708024aee6a2ea0f3176bdc2fd06887dad0
Anupam <itsmrad@gmail.com>
2025-05-12 14:00:00 +0530

    feat: redesign projects dashboard with collapsible sidebar and ripple showcase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { AppNavbar } from "@/components/app-navbar";
 import { ProjectsView } from "@/features/projects/components/projects-view";
 
 const Home = () => {
-  return <ProjectsView />
+  return (
+    <div className="h-screen flex flex-col overflow-hidden">
+      <AppNavbar />
+      <div className="flex-1 min-h-0 overflow-hidden">
+        <ProjectsView />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
